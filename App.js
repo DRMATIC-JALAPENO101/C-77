0757309b8239d2d430dd3640f286777957bb8fd5
DRMATIC-JALAPENO101 <77494119+DRMATIC-JALAPENO101@users.noreply.github.com>
2022-06-07 18:38:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import HomeScreen from "./screens/Home";
+import IssLocationScreen from "./screens/IssLocation";
+import MeteorScreen from "./screens/Meteors";
+
+export default function App() {
+  return ( 
+  <View style = {styles.container}>\
+      <HomeScreen/>
+      <IssLocationScreen/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
